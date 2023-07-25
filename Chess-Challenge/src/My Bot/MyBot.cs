4ea6697bd37e3f3fc34c02428c01163dd72ddc11
Kappa <f.cappetti.05@gmail.com>
2023-07-25 16:29:28 +0200

    Negamax

--- Chess-Challenge/src/My Bot/MyBot.cs
+++ Chess-Challenge/src/My Bot/MyBot.cs
@@ -4,99 +4,57 @@ public class MyBot : IChessBot
 {
     public Move Think(Board board, Timer timer)
     {
-        bool maximizing = board.IsWhiteToMove;
         Move[] legalMoves = board.GetLegalMoves();
+        Move bestMove = legalMoves[0];
 
-        if (maximizing)
+        int maxEval = int.MinValue;
+
+        foreach (Move move in legalMoves)
         {
-            int maxEval = int.MinValue;
-            Move bestMove = legalMoves[0];
+            board.MakeMove(move);
 
-            foreach (Move move in legalMoves)
+            int eval = -Search(board, 3);
+            if (eval > maxEval)
             {
-                board.MakeMove(move);
-
-                int eval = Search(board, 3, false);
-                if (eval > maxEval)
-                {
-                    maxEval = eval;
-                    bestMove = move;
-                }
-
-                board.UndoMove(move);
+                maxEval = eval;
+                bestMove = move;
             }
 
-            return bestMove;
+            board.UndoMove(move);
         }
-        else
-        {
-            int minEval = int.MaxValue;
-            Move bestMove = legalMoves[0];
 
-            foreach (Move move in legalMoves)
-            {
-                board.MakeMove(move);
-
-                int eval = Search(board, 3, true);
-                if (eval < minEval)
-                {
-                    minEval = eval;
-                    bestMove = move;
-                }
-
-                board.UndoMove(move);
-            }
-
-            return bestMove;
-        }
+        return bestMove;
     }
 
-    int Search(Board board, int depth, bool maximizing)
+    int Search(Board board, int depth)
     {
         if (depth == 0 || board.IsInCheckmate() || board.IsDraw())
-            return Evaluate(board);
+            // Return score relative to side to move
+            return (board.IsWhiteToMove ? 1 : -1) * Evaluate(board);
 
-        if (maximizing)
+        int maxEval = int.MinValue;
+
+        foreach (Move move in board.GetLegalMoves())
         {
-            int maxEval = int.MinValue;
+            board.MakeMove(move);
 
-            foreach (Move move in board.GetLegalMoves())
-            {
-                board.MakeMove(move);
+            int eval = -Search(board, depth - 1);
+            if (eval > maxEval)
+                maxEval = eval;
 
-                int eval = Search(board, depth - 1, false);
-                maxEval = System.Math.Max(maxEval, eval);
-
-                board.UndoMove(move);
-            }
-
-            return maxEval;
+            board.UndoMove(move);
         }
-        else
-        {
-            int minEval = int.MaxValue;
 
-            foreach (Move move in board.GetLegalMoves())
-            {
-                board.MakeMove(move);
-
-                int eval = Search(board, depth - 1, true);
-                minEval = System.Math.Min(minEval, eval);
-
-                board.UndoMove(move);
-            }
-
-            return minEval;
-        }
+        return maxEval;
     }
 
     int Evaluate(Board board)
     {
         return 200 * (board.GetPieceList(PieceType.King, true).Count - board.GetPieceList(PieceType.King, false).Count) +
-          9 * (board.GetPieceList(PieceType.Queen, true).Count - board.GetPieceList(PieceType.Queen, false).Count) +
-          5 * (board.GetPieceList(PieceType.Rook, true).Count - board.GetPieceList(PieceType.Rook, false).Count) +
-          3 * (board.GetPieceList(PieceType.Bishop, true).Count - board.GetPieceList(PieceType.Bishop, false).Count) +
-          3 * (board.GetPieceList(PieceType.Knight, true).Count - board.GetPieceList(PieceType.Knight, false).Count) +
-          board.GetPieceList(PieceType.Pawn, true).Count - board.GetPieceList(PieceType.Pawn, false).Count;
+            9 * (board.GetPieceList(PieceType.Queen, true).Count - board.GetPieceList(PieceType.Queen, false).Count) +
+            5 * (board.GetPieceList(PieceType.Rook, true).Count - board.GetPieceList(PieceType.Rook, false).Count) +
+            3 * (board.GetPieceList(PieceType.Bishop, true).Count - board.GetPieceList(PieceType.Bishop, false).Count) +
+            3 * (board.GetPieceList(PieceType.Knight, true).Count - board.GetPieceList(PieceType.Knight, false).Count) +
+            board.GetPieceList(PieceType.Pawn, true).Count - board.GetPieceList(PieceType.Pawn, false).Count;
     }
 }
